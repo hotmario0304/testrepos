9af90c57b7286a54563f34ed2a2343ca165fd9c9
hotmario <hotmario0304@infokeepers.net>
2020-07-24 08:48:56 +0900

    first commit

---
+++ morder-domain/src/main/java/jp/digitalchild/application/morder/ServletInitializer.java
@@ -0,0 +1,13 @@
+package jp.digitalchild.application.morder;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MorderDomainApplication.class);
+	}
+
+}
---
+++ morder-domain/src/test/java/jp/digitalchild/application/morder/MorderDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.digitalchild.application.morder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorderDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
